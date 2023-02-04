e1644e85d9105cd1b91e04c9c5406390f3f767af
Vladimir <dr.frisbi@gmail.com>
2023-02-04 16:23:35 +0700

    add code  041

---
+++ 041/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите какое количество чисел Вы хотите посчитать: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = CreateArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+ShowArray(array);
+Console.Write($"Количество чисел, которые больше нуля:{count}");
